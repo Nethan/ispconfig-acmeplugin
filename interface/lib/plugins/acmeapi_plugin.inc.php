7e72624b5016e45aaec1ee14c3018421c921ac4c
Johannes <hannes@cheat.at>
2025-04-29 13:22:37 +0200

    init

---
+++ interface/lib/plugins/acmeapi_plugin.inc.php
@@ -0,0 +1,197 @@
+<?php
+/*
+ * Copyright (c) 2025.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the name of ISPConfig nor the names of its contributors
+ *       may be used to endorse or promote products derived from this software without
+ *       specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @file        acmeapi_plugin.inc.php
+ * @author      Johannes Koschier <hannes@cheat.at>
+ *
+ */
+
+class acmeapi_plugin
+{
+    public string $plugin_name = 'acmeapi_plugin';
+    public string $class_name = 'acmeapi_plugin';
+
+    public string $plugin_dir;
+    private string $tableName = 'dns_soa';
+    private string $keyColumnName = 'plugin_acme_api_key';
+
+    public function __construct()
+    {
+        $this->plugin_dir = ISPC_ROOT_PATH . '/lib/plugins/' . $this->plugin_name;
+    }
+
+
+    public function onLoad(): void
+    {
+        global $app;
+
+        if ($this->checkDbColumnExist()) {
+            $app->plugin->registerEvent('dns:dns_soa:on_after_formdef', $this->plugin_name, 'dns_soa_form');
+            $app->plugin->registerEvent('dns:dns_soa:on_before_update', $this->plugin_name, 'dns_soa_form_on_before_update');
+        } elseif ($_SESSION["s"]["user"]["typ"] == 'admin') {
+            $app->plugin->registerEvent('dns:dns_soa:on_after_formdef', $this->plugin_name, 'dns_soa_form_notready');
+        }
+    }
+
+    public function dns_soa_form($event_name, $page_form): void
+    {
+        $this->loadLang($page_form);
+
+        $tabs = array (
+            'plugin_acmeapi' => array(
+            'title'  => "Plugin ACME api",
+            'width'  => 100,
+            'template'  => $this->plugin_dir . '/templates/acmeapi_plugin_soa_edit_tab.htm',
+            'fields'  => array (
+                'plugin_acmeapi_key' => array (
+                    'datatype' => 'VARCHAR',
+                    'formtype' => 'TEXT',
+                ),
+            )
+        ));
+
+        $this->insert($tabs, $page_form);
+    }
+
+    public function dns_soa_form_notready($event_name, $page_form): void
+    {
+        $this->loadLang($page_form);
+
+        $tabs = array (
+            'plugin_acmeapi' => array(
+                'title'  => "Plugin ACME api",
+                'width'  => 100,
+                'template'  => $this->plugin_dir . '/templates/acmeapi_plugin_soa_edit_tab_notready.htm',
+            ));
+
+        $this->insert($tabs, $page_form);
+    }
+
+    /*
+    public function dns_soa_form_on_before_update_disabled($event_name, $page_form): void {
+        global $app;
+        //print_r($_GET);exit;
+
+        if (!isset($_GET['acmetask'])) {
+            return; //Not the ACME Tab
+        }
+
+        if ($_GET['acmetask'] == 'create') {
+            $newApiKey = bin2hex(random_bytes(16)); //32 chars long
+        } elseif ($_GET['acmetask'] == 'delete') {
+            $newApiKey = NULL;
+        } else {
+            die("No valid task given - exiting"); // should never happen...
+        }
+        //echo "sddssdsdsdsdsd0".$newApiKey;exit;
+        $page_form->dataRecord['plugin_acmeapi_key'] = $newApiKey;
+    }
+
+    */
+    public function dns_soa_form_on_before_update($event_name, $page_form): void {
+        global $app;
+
+        $domain_id = $page_form->dataRecord['id'];
+        if (!is_numeric($domain_id)) {
+            die("Domain ID not numeric"); //should never happen...
+        }
+
+        if ($_SESSION["s"]["user"]["typ"] != 'admin') {
+            $clientGroupId = $_SESSION["s"]["user"]["default_group"];
+            $sql = "SELECT `id` FROM dns_soa WHERE `id` = ? AND sys_groupid = ?";
+            $res = $app->db->queryOneRecord($sql, $domain_id, $clientGroupId);
+            if (!is_array($res)) {
+                die("Domain with ID " . $domain_id . " not found or no permission"); //should never happen...
+            }
+        }
+        if ($_GET['acmetask'] == 'create') {
+            $newApiKey = bin2hex(random_bytes(16)); //32 chars long
+        } elseif ($_GET['acmetask'] == 'delete') {
+            $newApiKey = NULL;
+        } else {
+            die("No valid task given - exiting");
+        }
+
+
+        $sql = "UPDATE `dns_soa` SET `plugin_acmeapi_key` = ? WHERE `dns_soa`.`id` = ?";
+        $app->db->query($sql, $newApiKey, $domain_id); //Insert into dns_soa table
+        header("Location: /dns/dns_soa_edit.php?next_tab=plugin_acmeapi&id=$domain_id");
+        exit;
+    }
+
+    private function checkDbColumnExist()
+    {
+        global $app;
+
+        $sql = "SHOW COLUMNS FROM `dns_soa` WHERE FIELD IN('plugin_acmeapi_key')";
+        $result = $app->db->queryAllArray($sql);
+        if (! $result) {
+            return false;
+        }
+        return true;
+    }
+
+
+    private function loadLang($page_form): void
+    {
+        global $app, $conf;
+
+        $language = $app->functions->check_language(
+            $_SESSION['s']['user']['language'] ?? $conf['language']
+        );
+        $file = $this->plugin_dir . "/lib/lang/$language.lng";
+
+        if (!is_file($file)) {
+            $file = $this->plugin_dir . "/lib/lang/en.lng";
+        }
+
+        @include $file;
+        if (isset($page_form->wordbook) && isset($wb) && is_array($wb)) {
+
+            if (is_array($page_form->wordbook)) {
+                $page_form->wordbook = array_merge($page_form->wordbook, $wb);
+            } else {
+                $page_form->wordbook = $wb;
+            }
+        }
+    }
+
+    private function insert($tabs, $page_form): void
+    {
+        if (isset($page_form->formDef['tabs'])) {
+            $page_form->formDef['tabs'] += $tabs;
+        } elseif (isset($page_form->formDef['fields'])) {
+            foreach ($tabs as $tab) {
+                foreach ($tab['fields'] as $key => $value) {
+                    $page_form->formDef['fields'][$key] = $value;
+                }
+            }
+        }
+    }
+}
---
+++ interface/web/remote/plugin_acmeapi.php
@@ -0,0 +1,183 @@
+<?php
+
+
+define('USERNAME', 'ispconfigremoteuser');
+define('PASSWORD', 'XXXchangemeXXX');
+define('SOAP_LOCATION', 'https://localhost:8080/remote/index.php');
+define('SOAP_URI', 'https://localhost:8080/remote/');
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+
+
+if (isset($_SERVER['HTTP_AUTH_TOKEN'])) {
+    $authHeader = $_SERVER['HTTP_AUTH_TOKEN'];
+    $key = $authHeader;
+} else {
+    echo json_encode(["message" => "FAIL - No API Key 1"]);
+    exit;
+}
+
+$method = $_SERVER['REQUEST_METHOD'];
+$request = explode('/', trim($_SERVER['PATH_INFO'] ?? '', '/'));
+
+switch ($method) {
+    case 'POST':
+        if ($_SERVER['PATH_INFO'] == '/records') {
+            $input = json_decode(file_get_contents('php://input'), true);
+            $domain = $input['domain'];
+            $txt = $input['txt'];
+            create_record($domain, $txt, $key);
+            echo json_encode(["message" => "SUCCESS - TXT record added"]);
+        }
+        break;
+
+    case 'DELETE':
+        if ($_SERVER['PATH_INFO'] == '/records') {
+            $domain = $_GET['domain'] ?? null;
+            delete_record($domain,$key);
+            echo json_encode(["message" => "SUCCESS - TXT record removed"]);
+        }
+
+}
+
+function delete_record($record, $key) {
+    $con = apiConnect();
+    $session_id = $con['session_id'];
+    $client = $con['client'];
+
+    list($acmetxt, $url) = explode('.', $record, 2);
+    $parts = explode('.', $url);
+    while (count($parts) > 1) {
+        // Join the parts back into a string
+        $zone = implode('.', $parts);
+        try {
+            $zone_id = $client->dns_zone_get_id($session_id, $zone);
+            $zone_rec = $client->dns_zone_get($session_id, $zone_id);
+            $apikey = $zone_rec['plugin_acmeapi_key'];
+        } catch (SoapFault $e) { echo 'SOAP Error: ' . $e->getMessage(); }
+        if ($apikey == $key) { // found deepest Zone
+            break;
+        }
+        array_shift($parts);
+    }
+
+    // If no Zone with key found
+    if (!(is_numeric($zone_id))) {
+        echo json_encode(["message" => "FAIL - No zone found or key wrong"]);
+    }
+
+    $client_id = $client->client_get_id($session_id, $zone_rec['sys_userid']);
+    $server_id = $zone_rec['server_id'];
+
+    $apikey = $zone_rec['plugin_acmeapi_key'];
+    if ($apikey != $key) { //check again
+        echo json_encode(["message" => "FAIL - Wrong API Key for $zone"]);
+        exit;
+    }
+
+    $allRerecords = $client->dns_rr_get_all_by_zone($session_id, $zone_id);
+    $recordId = 0;
+    foreach ($allRerecords as $rr) {
+        if ($rr['zone'] == $zone_id && $rr['name'] == $record.'.') {
+            $recordId = $rr['id'];
+        }
+    }
+
+    if (!(is_numeric($recordId))) {
+        echo json_encode(["message" => "FAIL - No Record found"]);
+    }
+
+    try{
+        $client->dns_txt_delete($session_id, $recordId);
+        $client->logout($session_id);
+    } catch (SoapFault $e) {
+        die('SOAP Error: ' . $e->getMessage());
+        echo "Please contact the server administator";
+    }
+}
+
+function create_record($record,$txt,$key) {
+    $con = apiConnect();
+    $session_id = $con['session_id'];
+    $client = $con['client'];
+
+
+    list($acmetxt, $url) = explode('.', $record, 2);
+
+    $parts = explode('.', $url);
+    while (count($parts) > 1) {
+        // Join the parts back into a string
+        $zone = implode('.', $parts);
+        try {
+            $zone_id = $client->dns_zone_get_id($session_id, $zone);
+            $zone_rec = $client->dns_zone_get($session_id, $zone_id);
+            $apikey = $zone_rec['plugin_acmeapi_key'];
+        } catch (SoapFault $e) { }
+        if ($apikey == $key) { // found deepest Zone
+            break;
+        }
+        array_shift($parts);
+    }
+
+    // If no Zone with key found
+    if (!(is_numeric($zone_id))) {
+        echo json_encode(["message" => "FAIL - No Zone found or Key wrong"]);
+    }
+
+    $client_id = $client->client_get_id($session_id, $zone_rec['sys_userid']);
+    $server_id = $zone_rec['server_id'];
+
+    $apikey = $zone_rec['plugin_acmeapi_key'];
+    if ($apikey != $key) { //check again
+        echo json_encode(["message" => "FAIL - Wrong API Key for $zone"]);
+        exit;
+    }
+    try{
+        $params = [
+            'server_id' => $server_id,
+            'zone' => $zone_id,
+            'type' => 'TXT',
+            'name' => $record.'.',
+            'data' => $txt,
+            'ttl' => '900',
+            'aux' => '10',
+            'active' => 'y',
+        ];
+        $client->dns_txt_add($session_id, $client_id, $params, true);
+        $client->logout($session_id);
+
+    } catch (SoapFault $e) {
+        die('SOAP Error: ' . $e->getMessage());
+        echo "Please contact the server administator";
+    }
+}
+function apiConnect () {
+    $context = stream_context_create([
+        'ssl' => [
+            'verify_peer' => true,
+            'verify_peer_name' => false, //because localhost is not in SAN
+            'allow_self_signed' => false
+        ]
+    ]);
+
+    try {
+        $client = new SoapClient(null, array('location' => SOAP_LOCATION,
+                'uri' => SOAP_URI,
+                'stream_context' => $context)
+        );
+
+        //* Login to the remote server
+        if (!($session_id = $client->login(USERNAME, PASSWORD))) {
+            echo 'Loggin failed';
+        }
+    } catch (SoapFault $e) {
+        die('SOAP Error: ' . $e->getMessage());
+        echo "Please contact the server administator";
+    }
+    return ["session_id" => $session_id, "client" => $client ];
+}
+
+
+?>;
